c9809fec94e901e237471fc155cda26cf24980de
Mahideep Tumati <mahideep.tumati@gmail.com>
2021-11-10 04:21:23 -0500

    Chart visible

--- helper/script/helper.js
+++ helper/script/helper.js
@@ -24,6 +24,7 @@ lyrMarkerCluster = L.markerClusterGroup();
 let isOpenTodayCondition = false;
 
 var markerList = [];
+
 theMap.eachLayer(function (layer) {
   if (
     layer instanceof L.Marker &&
@@ -446,3 +447,56 @@ function getIcon(label, color) {
 }
 
 reloadMap();
+
+// foodDistributionTypes();
+
+// function fetchPieData() {
+//   console.log(indexReference);
+//   return data;
+// }
+// function foodDistributionTypes() {
+//   data = fetchPieData();
+//   fetch((width = svg.attr("width"))), (height = svg.attr("height"));
+
+//   const g = svg
+//     .append("g")
+//     .attr("transform", `translate(${width / 2}, ${height / 2})`);
+
+//   const color = d3.scaleOrdinal([
+//     "lightblue",
+//     "lightblue",
+//     "lightblue",
+//     "gray",
+//   ]);
+
+//   const pie = d3
+//     .pie()
+//     .sort(null)
+//     .value((d) => d.value);
+
+//   const path = d3.arc().outerRadius(radius).innerRadius(0);
+
+//   const label = d3
+//     .arc()
+//     .outerRadius(radius)
+//     .innerRadius(radius - 90);
+
+//   const pies = g
+//     .selectAll(".arc")
+//     .data(pie(data))
+//     .enter()
+//     .append("g")
+//     .attr("class", "arc");
+
+//   pies
+//     .append("path")
+//     .attr("d", path)
+//     .attr("fill", (d) => color(d.data.value));
+
+//   pies
+//     .append("text")
+//     .attr("transform", function (d) {
+//       return `translate(${label.centroid(d)})`;
+//     })
+//     .text((d) => d.data.name);
+// }
